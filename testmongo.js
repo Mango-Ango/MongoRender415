556a03db590a629f85668bbb43674d3d57947e0b
Mango-Ango <157052207+Mango-Ango@users.noreply.github.com>
2025-04-08 14:24:22 -0500

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,8 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://angusmckee:MangoMongo@cluster0.hbl2cs7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://angusmckee:MangoMongo@cluster0.hbl2cs7.mongodb.net/";
+  //mongodb+srv://angusmckee:MangoMongo@cluster0.hbl2cs7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
